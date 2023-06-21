b39f3f69b28c7c5acb1c636eddca1d5139883799
jirepos <jirepos@gmail.com>
2023-06-21 18:46:09 +0900

    설명서 작성

---
+++ src/lib/my-lib.js
@@ -0,0 +1,2 @@
+const version = "1.0"; 
+export { version}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<h1>about</h1>
